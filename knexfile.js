46d624d76a628511d1bf51d47308d7e7e202da6b
aaamg <andrewmanuelgarcia@gmail.com>
2019-11-15 16:56:20 -0500

    structure started, need to add tables, models, and seeds

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    // our DBMS driver
+    client: 'sqlite3',
+    // the location of our db
+    connection: {
+      filename: './data/database_file.db3',
+    },
+    // necessary when using sqlite3
+    useNullAsDefault: true,
+    // generates migration files in a data/migrations/ folder
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+}
