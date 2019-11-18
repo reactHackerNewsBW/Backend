19baa1ec06fea3eee5ba555a200e0ce1c237d814
aaamg <andrewmanuelgarcia@gmail.com>
2019-11-18 18:53:59 -0500

    ..

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 
-const configOptions = require('../knexfile').development;
+const configOptions = process.env.NODE_ENV === "production" ? require('../knexfile').production : require('../knexfile').development;
+
+// const configOptions = process.env.NODE_ENV === "" || "development" ? require('../knexfile').development : require('../knexfile').production;
 
 module.exports = knex(configOptions);
 
