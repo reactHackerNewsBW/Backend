a28293367b1d40255a3a1da1b5aa1dcc9007a7a0
aaamg <andrewmanuelgarcia@gmail.com>
2019-11-18 19:00:32 -0500

    .

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
       directory: './data/migrations'
     },
     production: {
-      client: 'postgres',
+      client: 'pg',
       connection: process.env.DATABASE_URL,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
