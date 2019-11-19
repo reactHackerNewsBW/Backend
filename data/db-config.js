61bc5a29e5a78f5635c65cd1b37170de5a1ef716
aaamg <andrewmanuelgarcia@gmail.com>
2019-11-18 19:13:42 -0500

    MVP

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const configOptions = require('../knexfile');
+const configOptions = require('../knexfile').development;
 
 //const configOptions = require('../knexfile').development;
 
